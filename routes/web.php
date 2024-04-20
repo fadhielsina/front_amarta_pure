9b647760ac93d685daa1b06022067929150a1deb
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2024-04-21 03:31:40 +0700

    routes update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // Amarta Pure
 Route::get('/', function () {
-    return view('amarta_pure/index');
+    return view("amarta_pure/index");
 });
 Route::get('/about-us', function () {
     return view('amarta_pure/aboutus');
