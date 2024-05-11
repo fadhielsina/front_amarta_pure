cc6f0c3472ab8fc33d1f72ae3af412d0895f0d75
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2024-05-12 00:04:30 +0700

    pakar page

--- resources/views/pure_water/pakar.blade.php
+++ resources/views/pure_water/pakar.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <section class="ttm-row ttm-bgcolor-grey clearfix">
     <div class="container">
-        <div class="row" style="margin-bottom: 20px;">
+        <div class="row">
             <div class="ttm-box-col-wrapper col-lg-6" style="margin-top: 20px;">
                 <!-- testimonials -->
                 <div class="testimonials ttm-testimonial-box-view-style2">
@@ -33,7 +33,7 @@
                         <blockquote class="testimonial-text">Air adalah materi dan matriks kehidupan.</blockquote>
                         <div class="testimonial-avatar">
                             <div class="testimonial-img">
-                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/albert.png" alt="testimonial-img">
+                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/albert.jpg" alt="testimonial-img">
                             </div>
                         </div>
                         <div class="testimonial-bottom">
@@ -56,7 +56,7 @@
                             hampir sempurna sebagian besar karena itu.</blockquote>
                         <div class="testimonial-avatar">
                             <div class="testimonial-img">
-                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/graham.png" alt="testimonial-img">
+                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/graham.jpg" alt="testimonial-img">
                             </div>
                         </div>
                         <div class="testimonial-bottom">
@@ -85,6 +85,24 @@
                         <div class="testimonial-bottom">
                             <div class="testimonial-caption">
                                 <h5>dr. otto heinrich warburg</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- testimonials end -->
+            </div>
+            <div class="ttm-box-col-wrapper col-lg-6" style="margin-top: 20px;">
+                <!-- testimonials -->
+                <div class="testimonials ttm-testimonial-box-view-style2">
+                    <div class="testimonial-content">
+                        <blockquote class="testimonial-text">air adalah raja dari semua obat</blockquote>
+                        <div class="testimonial-avatar">
+                            <div class="testimonial-img">
+                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/shizhen.jpg" alt="testimonial-img">
+                            </div>
+                        </div>
+                        <div class="testimonial-bottom">
+                            <div class="testimonial-caption">
+                                <h5>Li ShiZen</h5>
                             </div>
                         </div>
                     </div>
