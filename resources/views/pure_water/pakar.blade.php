1852ba8911fd598fdb8cfaeeee277d34aedfc4ef
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2024-05-11 23:40:15 +0700

    pakar page

--- resources/views/pure_water/pakar.blade.php
+++ resources/views/pure_water/pakar.blade.php
@@ -1,5 +1,97 @@
 @extends('pure_water.layouts.app')
 @section('content')
-
+<section class="ttm-row ttm-bgcolor-grey clearfix">
+    <div class="container">
+        <div class="row" style="margin-bottom: 20px;">
+            <div class="ttm-box-col-wrapper col-lg-6" style="margin-top: 20px;">
+                <!-- testimonials -->
+                <div class="testimonials ttm-testimonial-box-view-style2">
+                    <div class="testimonial-content">
+                        <blockquote class="testimonial-text">Insinyur, Ahli Mekanika,
+                            Matematikawan, Ahli perangkat
+                            10 bejana, kendi air dan bagian
+                            untuk di minum serta 5 mesin
+                            pengangkat sekaligus
+                            pemeliharaan air.</blockquote>
+                        <div class="testimonial-avatar">
+                            <div class="testimonial-img">
+                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/ismail.png" alt="testimonial-img">
+                            </div>
+                        </div>
+                        <div class="testimonial-bottom">
+                            <div class="testimonial-caption">
+                                <h5>Ismail Al Jazari</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- testimonials end -->
+            </div>
+            <div class="ttm-box-col-wrapper col-lg-6" style="margin-top: 20px;">
+                <!-- testimonials -->
+                <div class="testimonials ttm-testimonial-box-view-style2">
+                    <div class="testimonial-content">
+                        <blockquote class="testimonial-text">Air adalah materi dan matriks kehidupan.</blockquote>
+                        <div class="testimonial-avatar">
+                            <div class="testimonial-img">
+                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/albert.png" alt="testimonial-img">
+                            </div>
+                        </div>
+                        <div class="testimonial-bottom">
+                            <div class="testimonial-caption">
+                                <h5>Albert S. Gyorgyi</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- testimonials end -->
+            </div>
+            <div class="ttm-box-col-wrapper col-lg-6" style="margin-top: 20px;">
+                <!-- testimonials -->
+                <div class="testimonials ttm-testimonial-box-view-style2">
+                    <div class="testimonial-content">
+                        <blockquote class="testimonial-text">Saya tahu bahwa air suling murni. Saya pikir jika saya minum
+                            banyak, saya bisa menghilangkan beberapa garam yang
+                            menutupi saraf sciatic saya. Saya mencoba meminumnya
+                            meminumnya dan berhasil seperti pesona. Saya terus minum
+                            air suling dan saya menghubungkan kesehatan saya yang
+                            hampir sempurna sebagian besar karena itu.</blockquote>
+                        <div class="testimonial-avatar">
+                            <div class="testimonial-img">
+                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/graham.png" alt="testimonial-img">
+                            </div>
+                        </div>
+                        <div class="testimonial-bottom">
+                            <div class="testimonial-caption">
+                                <h5>Alexander Graham Bell</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- testimonials end -->
+            </div>
+            <div class="ttm-box-col-wrapper col-lg-6" style="margin-top: 20px;">
+                <!-- testimonials -->
+                <div class="testimonials ttm-testimonial-box-view-style2">
+                    <div class="testimonial-content">
+                        <blockquote class="testimonial-text">Sel kanker bersifat asam dan diberi gula,
+                            sedangkan sel sehat bersifat basa dan
+                            menghirup oksigen, sel kanker tidak dapat
+                            bertahan hidup dalam tubuh Basah ph 9+ Alkali
+                            yang kaya oksigen.
+                        </blockquote>
+                        <div class="testimonial-avatar">
+                            <div class="testimonial-img">
+                                <img class="img-fluid" src="{{url('pure_water')}}/images/inner_pages/testimonial/otto.png" alt="testimonial-img">
+                            </div>
+                        </div>
+                        <div class="testimonial-bottom">
+                            <div class="testimonial-caption">
+                                <h5>dr. otto heinrich warburg</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- testimonials end -->
+            </div>
+        </div><!-- row end -->
+    </div>
+</section>
 
 @endsection
